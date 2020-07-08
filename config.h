da79b4c19ed627f48a4f0dab4c65593c2a78e921
Hamza Mughal <hamza@prodesquare.com>
2020-07-08 19:28:10 +0500

    added opacity to statusbar

--- config.h
+++ config.h
@@ -23,10 +23,17 @@ static const char col_gray3[]       = "#bbbbbb";
 static const char col_gray4[]       = "#eeeeee";
 static const char col_cyan[]        = "#33415e";
 /* static const char col_cyan[]        = "#c70039"; */
+static const unsigned int baralpha = 0xc5;
+static const unsigned int borderalpha = OPAQUE;
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
 	[SchemeNorm] = { col_gray3, col_gray1, col_gray2 },
 	[SchemeSel]  = { col_gray4, col_cyan,  col_cyan  },
+};
+static const unsigned int alphas[][3]      = {
+	/*               fg      bg        border     */
+	[SchemeNorm] = { OPAQUE, baralpha, borderalpha },
+	[SchemeSel]  = { OPAQUE, baralpha, borderalpha },
 };
 
 /* tagging */
