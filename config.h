729bec25f13a83e05e5a8495d30252b5d9ee8cf0
Hamza Mughal <hamza@prodesquare.com>
2021-09-27 04:10:11 +0500

    gimp no more floating

--- config.h
+++ config.h
@@ -52,7 +52,7 @@ static const Rule rules[] = {
 	 /* *	WM_CLASS(STRING) = instance, class */
 	 /* *	WM_NAME(STRING) = title */
 	/* class		instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",		NULL,     NULL,           0,         1,          0,           0,        -1 },
+	{ "Gimp",		NULL,     NULL,           0,         0,          0,           0,        -1 },
 	{ "Firefox",	NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "St",			NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Alacritty",	NULL,     NULL,           0,         0,          1,           0,        -1 },
