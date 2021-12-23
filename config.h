0360c1166eda74986db91caeec1d8ef8aa999eb0
Hamza Mughal <hamza@prodesquare.com>
2021-12-24 02:41:10 +0500

    bar padding; outer gaps

--- config.h
+++ config.h
@@ -9,14 +9,14 @@ static const int swallowfloating    = 0;        /* 1 means swallow floating wind
 static const unsigned int gappih    = 10;       /* horiz inner gap between windows */
 static const unsigned int gappiv    = 10;       /* vert inner gap between windows */
 static const unsigned int gappoh    = 10;       /* horiz outer gap between windows and screen edge */
-static const unsigned int gappov    = 20;       /* vert outer gap between windows and screen edge */
+static const unsigned int gappov    = 10;       /* vert outer gap between windows and screen edge */
 static const int smartgaps          = 0;        /* 1 means no outer gap when there is only one window */
 
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
 
-static const int horizpadbar        = 5;        /* horizontal padding for statusbar */
-static const int vertpadbar         = 10;       /* vertical padding for statusbar */
+static const int horizpadbar        = 10;        /* horizontal padding for statusbar */
+static const int vertpadbar         = 15;       /* vertical padding for statusbar */
 static const int vertpad            = 0;		/* vertical padding of bar */
 static const int sidepad            = 0;		/* horizontal padding of bar */
 
@@ -28,7 +28,7 @@ static const char col_gray3[]       = "#d7dce2";
 static const char col_gray4[]       = "#ffd580";
 static const char col_cyan[]        = "#171c28";
 
-static const unsigned int baralpha = 0xfa;
+static const unsigned int baralpha = 0xd0;
 static const unsigned int borderalpha = OPAQUE;
 
 static const char *colors[][3]      = {
@@ -44,12 +44,11 @@ static const unsigned int alphas[][3]      = {
 };
 
 /* tagging */
-static const char *tags[] = { "1: ", "2: ", "3: 﫸", "4: ", "5: ", "6: ", "7: ", "8: ", "9: " };
+static const char *tags[] = { "1: ", "2: ", "3: 﫸", "4: ", "5: ", "6: ", "7: ", "8: ", "9: " };
+/* static const char *tags[] = { "1: ", "2: ", "3: 﫸", "4: ", "5: ", "6: ", "7: ", "8: ", "9: " }; */
 
 static const Rule rules[] = {
 	/* class		instance  title           tags mask  isfloating  isterminal  noswallow  monitor */
-	{ "Gimp",		NULL,     NULL,           0,         0,          0,           0,        -1 },
-	{ "Firefox",	NULL,     NULL,           1 << 8,    0,          0,          -1,        -1 },
 	{ "St",			NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ "Alacritty",	NULL,     NULL,           0,         0,          1,           0,        -1 },
 	{ NULL,			NULL,     "Event Tester", 0,         0,          0,           1,        -1 }, /* xev */
